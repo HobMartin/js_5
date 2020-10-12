9fb0580dcdfc43d9d3e5468ed8e9f11fae2135e9
HobMartin <ivanderda1999@gmail.com>
2020-10-13 01:59:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function computeAreaOfARectangle(a, b) {
+    return a * b;
+}
+
+var output = computeAreaOfARectangle(4,8);
+console.log(output);
+
+function computeAreaOfACircle(r) {
+    return Math.PI * Math.pow(r,2);
+}
+
+var output_1 = computeAreaOfACircle(4);
+console.log(output_1);
+
+function computePower(a, n) {
+    return Math.pow(a, n);
+}
+
+var output_2 = computePower(2, 3);
+console.log(output_2);
+
+function computeSquareRoot(n) {
+    return Math.sqrt(n);
+}
+
+var output_3 = computeSquareRoot(9);
+console.log(output_3);
